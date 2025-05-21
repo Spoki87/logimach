63fb43c121776e3bab0bbf6be2db318edc0d2398
Patryk Pawlak <patrykpawlak6789@gmail.com>
2025-05-21 18:51:55 +0200

    project initialization

---
+++ src/main/java/com/logimach/LogimachApplication.java
@@ -0,0 +1,13 @@
+package com.logimach;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogimachApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogimachApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/logimach/LogimachApplicationTests.java
@@ -0,0 +1,13 @@
+package com.logimach;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogimachApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
